706258add5cf50898b63c8a0118366c985fe54e7
Kieran Chambers <kieran.chambers@icloud.com>
2023-12-26 12:50:26 +0000

    Hide questions section on page load, and hide intro section and username submission on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,48 @@
-let questions = [
-  {
-    questionNum: 1,
-    questionText: "What is the answer?",
-    answer: "This",
-    answerChoices: ["answer 1", "answer 2", "This", "answer 4"],
-  },
-];
+document.addEventListener("DOMContentLoaded", function () {
+  // Question content displayed in the "questions" array
+  let questions = [
+    {
+      questionNum: 1,
+      questionText: "What is the answer?",
+      answer: "This",
+      answerChoices: ["answer 1", "answer 2", "This", "answer 4"],
+    },
+  ];
 
-// Setting gameDisplay variable to the 'question-area' div in index.html
-let gameDisplay = document.getElementById("question-area");
+  // Setting gameDisplay variable to the 'question-area' div in index.html
+  let gameSection = document.getElementById("question-area");
 
-// Accessing the questionText property from the first question object
-let questionText = questions[0].questionText;
+  // Accessing the questionText property from the first question object
+  let questionText = questions[0].questionText;
 
-// Assigning question HTML structure to the variable
-gameDisplay.innerHTML = `
-  <div id="question-area">
-    <p id="question-content">${questionText}</p>
-    <button class="default-button">${questions[0].answerChoices[0]}</button>
-    <button class="default-button">${questions[0].answerChoices[1]}</button>
-    <button class="default-button">${questions[0].answerChoices[2]}</button>
-    <button class="default-button">${questions[0].answerChoices[3]}</button>
-  </div>
-`;
+  // Assigning question HTML structure to the variable
+  gameSection.innerHTML = `
+      <div class="quit-buttons">
+        <button class="default-button">Pass</button>
+        <button class="default-button">Quit</button>
+      </div>
+      <div id="question-area-content">
+
+        <p id="question-content">${questionText}</p>
+        <button class="default-button answer-button">${questions[0].answerChoices[0]}</button>
+        <button class="default-button answer-button">${questions[0].answerChoices[1]}</button>
+        <button class="default-button answer-button">${questions[0].answerChoices[2]}</button>
+        <button class="default-button answer-button">${questions[0].answerChoices[3]}</button>
+      </div>
+    `;
+
+  // Assign the intro-section element to variable
+  let introSection = document.getElementById("intro-section");
+
+  // Get reference to the start button
+  let startButton = document.getElementById("start-button");
+
+  // Add a click event listener to the start button
+  startButton.addEventListener("click", function () {
+    // Hide the intro-section
+    introSection.style.display = "none";
+
+    // Show the game-section
+    gameSection.style.display = "block";
+  });
+});

f855fb95f9337ed633ce5fcf63160127d4a486df
Kieran Chambers <kieran.chambers@icloud.com>
2024-01-01 14:56:50 +0100

    Add nextQuestion() function and invoke this when user clicks pass button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Setting focus to username field on page load
 document.getElementById("username").focus();
 
+// Initialising i as 0
+let i = 0;
+
 // Waiting for DOM content to load before running this JavaScript
 document.addEventListener("DOMContentLoaded", function () {
   // Question content displayed in the "questions" array
@@ -19,40 +22,54 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     {
       questionNum: 3,
-      questionText: "Which of the following artists has the most streams on Spotify?",
+      questionText:
+        "Which of the following artists has the most streams on Spotify?",
       answer: "Drake",
       answerChoices: ["Madonna", "Drake", "Taylor Swift", "Kanye West"],
-    },{
+    },
+    {
       questionNum: 4,
       questionText: "How many minutes are in a full week?",
       answer: "10,080",
       answerChoices: ["10,080", "24,000", "11,857", "7,000"],
-    },{
+    },
+    {
       questionNum: 5,
-      questionText: "Brian May, guitarist of the band Queen, is also a specialist in which scientific field?",
+      questionText:
+        "Brian May, guitarist of the band Queen, is also a specialist in which scientific field?",
       answer: "Astrophysics",
-      answerChoices: ["Nuclear Physics", "Marine Biology", "Astrology", "Astrophysics"],
-    },{
+      answerChoices: [
+        "Nuclear Physics",
+        "Marine Biology",
+        "Astrology",
+        "Astrophysics",
+      ],
+    },
+    {
       questionNum: 6,
       questionText: "Aureolin is a shade of what color?",
       answer: "Yellow",
       answerChoices: ["Blue", "Green", "Black", "Yellow"],
-    },{
+    },
+    {
       questionNum: 7,
       questionText: "What country drinks the most coffee per capita?",
       answer: "Finland",
       answerChoices: ["United States", "Finland", "Italy", "Brazil"],
-    },{
+    },
+    {
       questionNum: 8,
       questionText: "How many bones are in the human ear?",
       answer: "3",
       answerChoices: ["12", "1", "3", "7"],
-    },{
+    },
+    {
       questionNum: 9,
       questionText: "Which planet has the most moons?",
       answer: "Saturn",
       answerChoices: ["Jupiter", "Saturn", "Uranus", "Earth"],
-    },{
+    },
+    {
       questionNum: 10,
       questionText: "How many elements are in the periodic table?",
       answer: "118",
@@ -70,11 +87,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // Assigning question HTML structure to the variable
   gameSection.innerHTML = `
       <div class="quit-buttons">
-        <button class="default-button id="pass-button"">Pass</button>
+        <button class="default-button" id="pass-button">Pass</button>
         <button class="default-button" id="quit-button">Quit</button>
       </div>
       <div id="question-area-content">
         <p>Current score: ${userScore}</p>
+        <br>
         <p id="question-content">${questionText}</p>
         <button class="default-button answer-button">${questions[0].answerChoices[0]}</button>
         <button class="default-button answer-button">${questions[0].answerChoices[1]}</button>
@@ -82,6 +100,30 @@ document.addEventListener("DOMContentLoaded", function () {
         <button class="default-button answer-button">${questions[0].answerChoices[3]}</button>
       </div>
     `;
+
+  function nextQuestion() {
+    console.log("Next question called");
+    // Check if there are more questions
+    if (i < questions.length - 1) {
+      // If so, show the next question
+      i++;
+      // Update questionText
+      questionText = questions[i].questionText;
+
+      // Update the question content
+      document.getElementById("question-content").textContent = questionText;
+
+      // Update answer choices
+      for (let j = 0; j < 4; j++) {
+        document.getElementsByClassName("answer-button")[j].textContent =
+          questions[i].answerChoices[j];
+      }
+    } else {
+      // If last question has been shown, hide the game display and show results
+      gameSection.style.display = "none";
+      resultsSection.style.display = "block";
+    }
+  }
 
   // Assign the intro-section element to variable
   let introSection = document.getElementById("intro-section");
@@ -116,12 +158,21 @@ document.addEventListener("DOMContentLoaded", function () {
   <button id="home-button" class="default-button answer-button" onClick="window.location.reload();">Play again</button></div>`;
 
   // Assign quit-button HTML element to quitButton variable
-  let quitButton = document.getElementById("quit-button");
+  const quitButton = document.getElementById("quit-button");
   // Add click event listener to quit button
   quitButton.addEventListener("click", function () {
+    console.log("Game quit");
     // Hide the games display
     gameSection.style.display = "none";
     // Show the results
     resultsSection.style.display = "block";
+  });
+
+  //Assigning pass-button HTML element to the passButton variable
+  const passButton = document.getElementById("pass-button");
+  // Adding click event listener to the pass button
+  passButton.addEventListener("click", function () {
+    console.log("Pass button clicked");
+    nextQuestion();
   });
 });
